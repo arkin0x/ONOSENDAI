77f9fe19e842c89d4280d9684ebb2e04f1f8c378
arkin0x <arkinox@pm.me>
2023-09-22 11:37:12 -0500

    wip

--- src/hooks/useCyberspaceStateReconciler.ts
+++ src/hooks/useCyberspaceStateReconciler.ts
@@ -39,9 +39,11 @@ const actionChainIsValid = (actions: ActionsState): boolean => {
     return false
   }))
 
-  // check the plane and make sure it is valid
-  // TODO: implement portals to switch planes; currently stuck on the starting plane
+  // the following validations only apply if we have more than 1 acton in the chain
   if (actions.length > 1) {
+
+    // check the plane and make sure it is valid
+    // TODO: implement portals to switch planes; currently stuck on the starting plane
     const testPlaneState = [...actions]
     const startPlane = getPlaneFromAction(testPlaneState[0])
     const planeIsValid = testPlaneState.reduce<false | 'd-space' | 'c-space'>((plane, action) => {
@@ -54,11 +56,9 @@ const actionChainIsValid = (actions: ActionsState): boolean => {
       }
     }, startPlane)
     tests.push(!!planeIsValid)
-  }
 
-  // check the velocity and make sure it is within tolerances
-  const testVelocityState = [...actions]
-  if (testVelocityState.length > 1) {
+    // check the velocity and make sure it is within tolerances
+    const testVelocityState = [...actions]
     // running simulated velocity
     const velocity: THREE.Vector3 = new THREE.Vector3(0,0,0)
     tests.push(testVelocityState.every((action, index) => {
@@ -67,35 +67,44 @@ const actionChainIsValid = (actions: ActionsState): boolean => {
         // this action's velocity should match the velocity simulation
         const v = new THREE.Vector3().fromArray(action.tags.find(tag => tag[0] === 'velocity')!.slice(1).map(parseFloat))
         if (!vector3Equal(v, velocity)) {
-          return false
           // dump action chain
+          return false
         }
-        // simulate velocity until next action
+
+        // if this is the last action, we're done
+        if (index === testVelocityState.length - 1) {
+          return true
+        }
+
+        // if this is not the last action, simulate velocity until next action
+
         // 1. add POW as velocity on quaternion
         if (action.tags.find(tag => tag[0] === 'A' && tag[1] === 'drift')) {
           // quaternion from the action
           const q = new THREE.Quaternion().fromArray(action.tags.find(tag => tag[0] === 'quaternion')!.slice(1).map(parseFloat))
           // add POW to velocity for drift event
           const POW = countLeadingZeroes(action.id)
-          const bodyVelocity = new THREE.Vector3(0,0,POW)
+          const newVelocity = Math.pow(2, POW)
+          const bodyVelocity = new THREE.Vector3(0,0,newVelocity)
           const addedVelocity = bodyVelocity.applyQuaternion(q)
           velocity.add(addedVelocity)
         }
 
         // 2. simulate velocity for each frame up to and not including the next action.
-        // drag is not applied to the last frame because the next action will calculate starting at its own timestamp.
         // timestamp with ms
-        const start_ts = parseInt(action.tags.find(tag => tag[0] === 'ms')![1]) + action.created_at
+        const start_ts = parseInt(action.tags.find(tag => tag[0] === 'ms')![1]) + action.created_at * 1000 // if there is an error parsing the ms tag, the catch will invalidate the action chain
         // get next action so we can simulate velocity changes between this action and next action.
         const nextAction = testVelocityState[index+1]
         // next action timestamp with ms
-        const end_ts = parseInt(nextAction.tags.find(tag => tag[0] === 'ms')![1]) + nextAction.created_at
+        const end_ts = parseInt(nextAction.tags.find(tag => tag[0] === 'ms')![1]) + nextAction.created_at * 1000
 
+        // Using Math.floor() because drag is not applied to the last frame. The next action will calculate drag starting at its own timestamp. The time between the last frame and the next action is variable and will be less than a frame. Technically, operators are penalized with more drag over time if this duration is shorter, and optimizing the length of time between actions is a strategy for maximizing velocity. This is not a bug; to keep things simple we restart the frame clock with each new action, which has the side effect of making this overlap possible.
         let iterations = Math.floor((end_ts - start_ts) / FRAME)
         while ( iterations--) {
           velocity.multiplyScalar(0.999)
         }
         // done simulating velocity. we'll see if it matches the next action in the next iteration of the loop.
+        return true
       } catch (error) {
         console.error(error)
         return false

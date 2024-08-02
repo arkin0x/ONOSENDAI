39da18ee562b202f35ba3c545fde307d91407025
arkin0x <arkinox@pm.me>
2024-08-01 23:12:49 -0500

    smooth trail

--- src/components/ThreeAvatarTrail.tsx
+++ src/components/ThreeAvatarTrail.tsx
@@ -2,7 +2,8 @@ import { useContext, useMemo } from 'react'
 import { Vector3, Color } from 'three'
 import { Line } from '@react-three/drei'
 import { AvatarContext } from '../providers/AvatarContext'
-import { extractActionState } from '../libraries/Cyberspace'
+import { extractActionState, getSectorCoordinatesFromCyberspaceCoordinates } from '../libraries/Cyberspace'
+import { spawn } from 'child_process'
 
 interface ThreeAvatarTrailProps {
   pubkey: string
@@ -11,14 +12,17 @@ interface ThreeAvatarTrailProps {
 export function ThreeAvatarTrail({ pubkey }: ThreeAvatarTrailProps) {
   const { actionState } = useContext(AvatarContext)
 
+  const spawnPosition = getSectorCoordinatesFromCyberspaceCoordinates(pubkey).toVector3()
+
   const trailPoints = useMemo(() => {
     const actions = actionState[pubkey] || []
     if (actions.length < 2) return [] // We need at least 2 points to draw a line
 
     return actions.map(action => {
       const { sectorPosition } = extractActionState(action)
-      console.log('trail', sectorPosition.toVector3())
-      return new Vector3(sectorPosition.x.toNumber(), sectorPosition.y.toNumber(), sectorPosition.z.toNumber())
+      // I need to subtract the spawn position to get the relative position
+      const newVec = sectorPosition.toVector3().sub(spawnPosition)
+      return newVec
     })
   }, [actionState, pubkey])
 
@@ -27,10 +31,12 @@ export function ThreeAvatarTrail({ pubkey }: ThreeAvatarTrailProps) {
   }
 
   return (
-    <Line
-      points={trailPoints}
-      color={new Color(0xff2323)}
-      lineWidth={1}
-    />
+    <group position={spawnPosition}>
+      <Line
+        points={trailPoints}
+        color={new Color(0xff2323)}
+        lineWidth={1}
+      />
+    </group>
   )
 }

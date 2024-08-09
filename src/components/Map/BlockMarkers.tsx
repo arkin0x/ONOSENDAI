4dbe827a51791e4a53413ffece0920500e576638
arkin0x <arkinox@pm.me>
2024-08-09 17:59:21 -0500

    connect blocks

--- src/components/Map/BlockMarkers.tsx
+++ src/components/Map/BlockMarkers.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext, useMemo } from 'react'
 import { NDKContext } from '../../providers/NDKProvider'
-import { BufferGeometry, Float32BufferAttribute, PointsMaterial, Vector3 } from 'three'
+import { BufferGeometry, Vector3, LineBasicMaterial } from 'three'
 import { CYBERSPACE_AXIS, decodeHexToCoordinates } from '../../libraries/Cyberspace'
 import { NDKEvent, NDKFilter } from '@nostr-dev-kit/ndk'
 
@@ -20,34 +20,31 @@ export const BlockMarkers: React.FC<BlocksProps> = ({ scale }) => {
       let filter: NDKFilter
 
       if (blocks.length === 0) {
-        // Fetch the first block
         filter = {
           kinds: [321],
           limit: 1,
           "#P": ["0000000000000000000000000000000000000000000000000000000000000000"]
         }
       } else {
-        // Fetch the next block
-    const nextBlockHash = blocks[blocks.length - 1].tags.find(tag => tag[0] === 'N')?.[1]
+        const nextBlockHash = blocks[blocks.length - 1].tags.find(tag => tag[0] === 'N')?.[1]
         if (!nextBlockHash) {
           console.error('Block event is missing N tag')
           return
         }
         filter = {
           kinds: [321],
-        limit: 1,
-        "#H": [nextBlockHash]
+          limit: 1,
+          "#H": [nextBlockHash]
         }
       }
 
       try {
-      const event = await ndk.fetchEvent(filter)
+        const event = await ndk.fetchEvent(filter)
         if (event) {
           setBlocks(prevBlocks => [...prevBlocks, event])
-          // Trigger next fetch after a short delay
           setTimeout(() => setFetchCounter(prev => prev + 1), 1000)
-      } else {
-          console.log('No more blocks to fetch. Last block fetched:' , blocks[blocks.length - 1].tags.find(tag => tag[0] === 'H')?.[1])
+        } else {
+          console.log('No more blocks to fetch')
         }
       } catch (error) {
         console.error('Failed to fetch block:', error)
@@ -60,37 +57,56 @@ export const BlockMarkers: React.FC<BlocksProps> = ({ scale }) => {
   return (
     <>
       {blocks.map((block, index) => (
-        <Block key={index} event={block} scale={scale} />
+        <BlockLine 
+          key={index} 
+          currentBlock={block} 
+          nextBlock={blocks[index + 1]} 
+          scale={scale} 
+        />
       ))}
     </>
   )
 }
 
-interface BlockProps {
-  event: NDKEvent
+interface BlockLineProps {
+  currentBlock: NDKEvent
+  nextBlock?: NDKEvent
   scale: number
 }
 
-const Block: React.FC<BlockProps> = ({ event, scale }) => {
-  const position = getBlockPosition(event, scale)
-  const size = getBlockSize(event)
+const BlockLine: React.FC<BlockLineProps> = ({ currentBlock, nextBlock, scale }) => {
+  const startPosition = useMemo(() => getBlockPosition(currentBlock, scale), [currentBlock, scale])
+  const endPosition = useMemo(() => nextBlock ? getBlockPosition(nextBlock, scale) : null, [nextBlock, scale])
 
   const geometry = useMemo(() => {
     const geo = new BufferGeometry()
-    geo.setAttribute('position', new Float32BufferAttribute([position.x, position.y, position.z], 3))
+    if (endPosition) {
+      const points = [startPosition, endPosition]
+      geo.setFromPoints(points)
+    } else {
+      // If there's no end position, just create a point at the start
+      geo.setFromPoints([startPosition])
+    }
     return geo
-  }, [position])
+  }, [startPosition, endPosition])
 
   const material = useMemo(() => {
-    return new PointsMaterial({
+    return new LineBasicMaterial({
       color: 0xff9900,
-      size: 2,
-      sizeAttenuation: false
+      linewidth: 2,
     })
-  }, [size])
+  }, [])
 
-  return <points geometry={geometry} material={material} />
+  if (!endPosition) {
+    // If there's no end position, don't render anything
+    return null
+  }
 
+  return (
+    <mesh>
+      <line geometry={geometry} material={material} />
+    </mesh>
+  )
 }
 
 function getBlockPosition(event: NDKEvent, scale: number): Vector3 {
@@ -104,11 +120,4 @@ function getBlockPosition(event: NDKEvent, scale: number): Vector3 {
   const position = decodeHexToCoordinates(coordinate)
   const scaledPosition = position.vector.divideScalar(CYBERSPACE_AXIS.div(scale))
   return scaledPosition.toVector3()
-}
-
-function getBlockSize(event: NDKEvent): number {
-  // Extract size from event tags or content
-  // This is a placeholder implementation
-  // You might want to base this on some property of the event
-  return 3 // Fixed size of 5 units
 }

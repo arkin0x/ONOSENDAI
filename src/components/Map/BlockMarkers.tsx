23e7db20e7710002202e17b62d06a83bf0ec8f90
arkin0x <arkinox@pm.me>
2024-09-10 22:22:11 -0500

    WIP add plane toggle to grid for earth and alt colors

--- src/components/Map/BlockMarkers.tsx
+++ src/components/Map/BlockMarkers.tsx
@@ -16,7 +16,7 @@ export const BlockMarkers: React.FC<BlocksProps> = ({ scale }) => {
 
   useEffect(() => {
     if (!ndk) return
-    if (blocks.length > 100) return // abritrary limit that should be replaced with good caching.
+    if (blocks.length > 10) return // abritrary limit that should be replaced with good caching.
 
     const fetchNextBlock = async () => {
       let filter: NDKFilter
--- src/components/Map/Grid.tsx
+++ src/components/Map/Grid.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react"
 import { Euler } from "three"
 import COLORS from "../../data/Colors"
+import { CyberspacePlane } from "../../libraries/Cyberspace"
 
 const GRIDLINES = 8
 const EARTH_SCALE = 0.13264 // Proportion of Earth's diameter to D-Space axis
 
-export function Grid({children, scale}: {children?: ReactNode, scale: number}) {
+export function Grid({children, scale, plane = CyberspacePlane.DSpace}: {children?: ReactNode, scale: number, plane: CyberspacePlane}) {
   const earthRadius = (scale * EARTH_SCALE) / 2
   return (
     <group
@@ -13,29 +14,35 @@ export function Grid({children, scale}: {children?: ReactNode, scale: number}) {
       position={[0, 0, 0]}
     >
       <gridHelper // Top Grid
-        args={[scale, GRIDLINES, COLORS.GRID_CROSS, COLORS.SKY]}
+        args={[scale, GRIDLINES, COLORS.GRID_CROSS, plane === CyberspacePlane.DSpace ? 0x3B0097 : COLORS.SKY]}
         position={[scale/2, scale, scale/2]} // all coordinates are positive, so the top left corner of the grid should be x0 z0.
       />
       <gridHelper // Bottom Grid
-        args={[scale, GRIDLINES, COLORS.GRID_CROSS, COLORS.GROUND]}
+        args={[scale, GRIDLINES, COLORS.GRID_CROSS, plane === CyberspacePlane.DSpace ? 0x3A0C40 : COLORS.GROUND]}
         position={[scale/2, 0, scale/2]} // all coordinates are positive, so the top left corner of the grid should be x0 z0.
       />
-      <mesh // Black Sun
+      { plane === CyberspacePlane.ISpace ? <mesh // Black Sun
         position={[scale/2, scale/2, -scale/2]}
         renderOrder={-1}
       >
         <circleGeometry args={[scale/4]} />
         <meshBasicMaterial color={COLORS.BLACK_SUN} />
-      </mesh>
-      <mesh position={[scale/2, scale/2, scale/2]}>
-        <sphereGeometry args={[earthRadius, 64, 64]} />
-        <meshPhongMaterial color={COLORS.BLUE} opacity={.8} transparent={true} />
-      </mesh>
+      </mesh> : null }
 
-      <lineSegments position={[scale/2, scale/2, scale/2]}>
-        <icosahedronGeometry args={[earthRadius, 1]} />
-        <lineBasicMaterial color={COLORS.BLUE } />
-      </lineSegments>
+      {/* display Earth in d-space */}
+      { plane === CyberspacePlane.DSpace ? 
+        <>
+          <mesh position={[scale/2, scale/2, scale/2]}>
+            <sphereGeometry args={[earthRadius, 64, 64]} />
+            <meshPhongMaterial color={COLORS.BLUE} opacity={.8} transparent={true} />
+          </mesh>
+          <lineSegments position={[scale/2, scale/2, scale/2]}>
+            <icosahedronGeometry args={[earthRadius, 1]} />
+            <lineBasicMaterial color={COLORS.BLUE } />
+          </lineSegments>
+        </>
+      : null }
+
       {children}
     </group>
   )

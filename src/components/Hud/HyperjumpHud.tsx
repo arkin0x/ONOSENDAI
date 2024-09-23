4f5d9ed34ccee421f66636cd6c872b399a022397
arkin0x <arkinox@pm.me>
2024-09-23 09:20:06 -0500

    distance to hyperjump eta to hyperjump speed

--- src/components/Hud/HyperjumpHud.tsx
+++ src/components/Hud/HyperjumpHud.tsx
@@ -1,15 +1,17 @@
 import { Text } from "@react-three/drei"
-import { useThree } from "@react-three/fiber"
-import { useEffect, useState } from "react"
+import { useFrame, useThree } from "@react-three/fiber"
+import { useEffect, useRef, useState } from "react"
 import { Quaternion, Vector3 } from "three"
 import COLORS from "../../data/Colors"
 import { useSectorStore } from "../../store/SectorStore"
 import { Event } from 'nostr-tools'
 import { useAvatarStore } from "../../store/AvatarStore"
 import useNDKStore from "../../store/NDKStore"
-import { cyberspaceCoordinateFromHexString, CyberspaceLocalCoordinate, extractCyberspaceActionState } from "../../libraries/Cyberspace"
+import { cyberspaceCoordinateFromHexString, CyberspaceLocalCoordinate, extractCyberspaceActionState, ExtractedCyberspaceActionState } from "../../libraries/Cyberspace"
 import { getTag } from "../../libraries/NostrUtils"
 import { useRotationStore } from "../../store/RotationStore"
+import { distance } from "three/examples/jsm/nodes/Nodes.js"
+import { convertSeconds } from "../../libraries/utils"
 
 
 export const HyperjumpHud = () => {
@@ -21,41 +23,68 @@ export const HyperjumpHud = () => {
   const [hyperjump, setHyperjump] = useState<[Event,CyberspaceLocalCoordinate]>()
   const [pointDirection, setPointDirection] = useState<Quaternion>()
   const { rotation } = useRotationStore()
+  const distanceRef = useRef<number>(0)
+  const simulatedStateRef = useRef<ExtractedCyberspaceActionState>()
+
+  const { camera } = useThree()
 
   // Effect: get closest hyperjump to avatar
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (userCurrentSectorId && sectorState[userCurrentSectorId].hyperjumps.length > 0) {
-        // get avatar local position
-        const avatarSimState = getSimulatedState(pubkey)
-        if (!avatarSimState) return
-        const avatarCoord = extractCyberspaceActionState(avatarSimState).localCoordinate
-        const hyperjumpCoords: [Event,CyberspaceLocalCoordinate][] = []
-        // get hyperjumps local positions
-        for (const hyperjump of sectorState[userCurrentSectorId].hyperjumps) {
-          try {
-            const hyperjumpHex = hyperjump.tags.find(getTag('C'))![1]
-            const hyperjumpCoord = cyberspaceCoordinateFromHexString(hyperjumpHex).local
-            hyperjumpCoords.push([hyperjump, hyperjumpCoord])
-          } catch (e) {
-            console.error(e)
-          }
+    if (userCurrentSectorId && sectorState[userCurrentSectorId].hyperjumps.length > 0) {
+      // get avatar local position
+      const avatarSimState = getSimulatedState(pubkey)
+      if (!avatarSimState) return
+      const avatarCoord = extractCyberspaceActionState(avatarSimState).localCoordinate
+      const hyperjumpCoords: [Event,CyberspaceLocalCoordinate][] = []
+      // get hyperjumps local positions
+      for (const hyperjump of sectorState[userCurrentSectorId].hyperjumps) {
+        try {
+          const hyperjumpHex = hyperjump.tags.find(getTag('C'))![1]
+          const hyperjumpCoord = cyberspaceCoordinateFromHexString(hyperjumpHex).local
+          hyperjumpCoords.push([hyperjump, hyperjumpCoord])
+        } catch (e) {
+          console.error(e)
         }
-        // sort hyperjumps by distance to avatar
-        hyperjumpCoords.sort((a,b) => b[1].vector.toVector3().distanceTo(avatarCoord.vector.toVector3()) - a[1].vector.toVector3().distanceTo(avatarCoord.vector.toVector3()))
-        // set the closest hyperjump to the state
-        setHyperjump(hyperjumpCoords[0])
-        // calculate the quaternion for the cone's rotation to point from the avatar position toward the closest hyperjump
-        const direction = hyperjumpCoords[0][1].vector.toVector3().sub(avatarCoord.vector.toVector3()).normalize()
-        const defaultDirection = new Vector3(0, 1, 0) // Assuming the cone points along the z-axis by default
-        const quat = new Quaternion().setFromUnitVectors(defaultDirection, direction);
-        const combinedQuat = new Quaternion().multiplyQuaternions(rotation, quat)
-        setPointDirection(combinedQuat)
       }
-    }, 50) // check every 5 seconds
+      // sort hyperjumps by distance to avatar
+      hyperjumpCoords.sort((a,b) => b[1].vector.toVector3().distanceTo(avatarCoord.vector.toVector3()) - a[1].vector.toVector3().distanceTo(avatarCoord.vector.toVector3()))
+      // set the closest hyperjump to the state
+      setHyperjump(hyperjumpCoords[0])
+      // calculate the quaternion for the cone's rotation to point from the avatar position toward the closest hyperjump
+      // const direction = hyperjumpCoords[0][1].vector.toVector3().sub(avatarCoord.vector.toVector3()).normalize()
+      // const defaultDirection = new Vector3(0, 1, 0) // Assuming the cone points along the z-axis by default
+      // const quat = new Quaternion().setFromUnitVectors(defaultDirection, direction);
+      // const combinedQuat = new Quaternion().multiplyQuaternions(rotation, quat)
+      // setPointDirection(combinedQuat)
+    }
+  }, [userCurrentSectorId, sectorState, getSimulatedState, pubkey])
 
-    return () => clearInterval(interval)
-  }, [userCurrentSectorId, sectorState, getSimulatedState, pubkey, rotation])
+  // Effect: update distance to hyperjump each frame
+  useFrame(() => {
+    // store the simulated state
+    const simulated = getSimulatedState(pubkey)
+    if (simulated) {
+      simulatedStateRef.current = extractCyberspaceActionState(simulated)
+    }
+    // store the distance to the hyperjump
+    if (hyperjump) {
+      const avatarSimState = getSimulatedState(pubkey)
+      if (!avatarSimState) return
+      const avatarCoord = extractCyberspaceActionState(avatarSimState).localCoordinate
+      const distance = hyperjump[1].vector.toVector3().distanceTo(avatarCoord.vector.toVector3())
+      distanceRef.current = distance
+    }
+  })
+
+  let speed: number = 0 
+  if (simulatedStateRef.current){
+    const velocity = simulatedStateRef.current.velocity;
+    speed = Math.sqrt(velocity.x.pow(2).add(velocity.y.pow(2).add(velocity.z.pow(2))).toNumber()) * 60
+  }
+
+  const eta = speed ? Math.floor(distanceRef.current / (speed)) : null
+
+  const etaParts = convertSeconds(eta || Infinity)
 
   const x = 99
   // const r = Math.PI / 5 // rotation
@@ -64,7 +93,7 @@ export const HyperjumpHud = () => {
   const divisor = Math.max(4, Math.floor(windowWidth / 600))
   const r = -Math.PI / divisor // rotation
 
-  let line = 20
+  let line = 4
 
   const nextLine = () => {
     line += 2
@@ -79,8 +108,11 @@ export const HyperjumpHud = () => {
   return (
     <>
     <group>
+      <CoordinateText position={{x, y: nextLine()}} rotation={[0, r, 0]} text={`${(speed||0).toFixed(2)} G/s`} align="right" color={COLORS.ORANGE} />
+      { eta && <CoordinateText position={{x, y: nextLine()}} rotation={[0, r, 0]} text={`${etaParts.days}d ${etaParts.hours}h ${etaParts.minutes}m ${etaParts.seconds}s`} align="right" color={COLORS.ORANGE} /> }
+      <CoordinateText position={{x, y: nextLine()}} rotation={[0, r, 0]} text={`${distanceRef.current.toFixed(speed > 1000 ? 0 : 2)} Gibsons`} align="right" color={COLORS.YELLOW} />
       <CoordinateText position={{x, y: nextLine()}} rotation={[0, r, 0]} text={'LOCAL HYPERJUMP'} align="right" color={COLORS.YELLOW} />
-      {pointDirection && <DirectionCone x={90} y={nextLine()} rotation={[0, r, 0]} pointDirection={pointDirection} />}
+      {pointDirection && <DirectionCone x={50} y={50} rotation={[0, 0, 0]} pointDirection={pointDirection} />}
     </group>
     </>
   )
--- src/libraries/utils.ts
+++ src/libraries/utils.ts
@@ -5,3 +5,22 @@ export function pickRandom<T>(iterable: Iterable<T>): T {
   const index = Math.floor(Math.random() * array.length)
   return array[index]
 }
+
+// Example usage:
+// const result = convertSeconds(987654)
+// console.log(result); // { days: 11, hours: 10, minutes: 44, seconds: 14 }
+export function convertSeconds(totalSeconds: number) {
+  const days = Math.floor(totalSeconds / (24 * 3600));
+  totalSeconds %= 24 * 3600;
+  const hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  return {
+    days: days,
+    hours: hours.toString().padStart(2, '0'),
+    minutes: minutes.toString().padStart(2, '0'),
+    seconds: seconds.toString().padStart(2, '0')
+  };
+}

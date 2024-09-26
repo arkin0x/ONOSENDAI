ce3130fc3ce52ad5669de55607dcd8845ae9e65c
arkin0x <arkinox@pm.me>
2024-09-25 22:10:36 -0500

    add terminal text

--- src/components/Info/CyberspaceInfo.tsx
+++ src/components/Info/CyberspaceInfo.tsx
@@ -1,59 +1,27 @@
-import { useContext, useEffect, useState } from 'react'
-import { Canvas, useThree } from "@react-three/fiber"
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from "@react-three/fiber"
 import "../../scss/CyberspaceViewer.scss"
-import { Fog, Vector3 } from 'three'
+import { Fog } from 'three'
 // import { BlockMarkers } from './BlockMarkers'
 // import { Constructs } from './Constructs'
 // import { ObjectMarkers } from './ObjectMarkers'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
-import { OrbitControls, Text } from '@react-three/drei'
-import { useMapCenterSectorStore } from '../../store/MapCenterSectorStore'
-import { Grid } from '../Map/Grid'
-import { Hyperjumps } from '../Map/Hyperjumps'
-import { ThreeAvatarMarker } from '../Map/ThreeAvatarMarker'
-import { useAvatarStore } from '../../store/AvatarStore'
-import { CYBERSPACE_AXIS, CyberspacePlane, extractCyberspaceActionState } from '../../libraries/Cyberspace'
-import useNDKStore from '../../store/NDKStore'
+import { OrbitControls } from '@react-three/drei'
 import { CoordinateText } from '../Hud/CoordinateText'
 import COLORS from '../../data/Colors'
-// import SectorCrawler from './SectorCrawler'
-
-const MAP_SIZE = 100
 
 export type CyberspaceViewerProps = {
   style?: React.CSSProperties,
 }
 
-const CyberspaceGlobal = ({style = {height: "100svh"}}: CyberspaceViewerProps) => {
-  const { getUser } = useNDKStore()
-  const identity = getUser()
-  const { getSimulatedState } = useAvatarStore()
-  const { centerSectorId } = useMapCenterSectorStore()
-  const [avatarPosition, setAvatarPosition] = useState(new Vector3(0,0,0))
-  const [avatarPlane, setAvatarPlane] = useState<CyberspacePlane>(CyberspacePlane.DSpace)
-
-  const pubkey = identity!.pubkey
-
-  useEffect(() => {
-    const simulatedEvent = getSimulatedState(pubkey)
-    if (!simulatedEvent) return
-    const { coordinate, plane } = extractCyberspaceActionState(simulatedEvent)
-    setAvatarPlane(plane)
-    const avatarPosition = coordinate.vector.divideScalar(CYBERSPACE_AXIS).multiplyScalar(MAP_SIZE).toVector3()
-    setAvatarPosition(avatarPosition)
-  }, [pubkey, getSimulatedState])
-
+const CyberspaceInfo = ({style = {height: "100svh"}}: CyberspaceViewerProps) => {
   return (
     <div className="cyberspace-info">
       <div id="info">
         <Canvas style={style}>
           <ambientLight intensity={2.0} />
           <OrbitControls />
-          <CoordinateText
-            position={{x: 50, y: 50}}
-            color={COLORS.BITCOIN}
-            text={"test"}
-          />
+          <Terminal />
           <EffectComposer>
             <Bloom mipmapBlur levels={9} intensity={20} luminanceThreshold={0.001} luminanceSmoothing={0} />
           </EffectComposer>
@@ -63,16 +31,33 @@ const CyberspaceGlobal = ({style = {height: "100svh"}}: CyberspaceViewerProps) =
   )
 }
 
-export default CyberspaceGlobal 
+export default CyberspaceInfo 
 
-function MapCamera() {
-  const { camera, scene } = useThree()
-  useEffect(() => {
-    const fogColor = 0x000000 // Color of the fog
-    const far = 1000
-    scene.fog = new Fog(fogColor, 1, far / 2)
-    camera.far = far
-    camera.updateProjectionMatrix()
-  }, [camera])
-  return null
+function Terminal() {
+  const cursorOnChar = ">"
+  const cursorOffChar = "\u00A0"
+  const cursorBlinkRate = 30
+  const cursorTimerReset = 60
+  const [cursor, setCursor] = useState(cursorOnChar)
+  const cursorRef = useRef(cursorTimerReset)
+
+  useFrame(() => {
+    cursorRef.current -= 1
+    if (cursorRef.current <= 0) {
+      cursorRef.current = cursorTimerReset
+      setCursor(cursorOnChar)
+    }
+    if (cursorRef.current < cursorBlinkRate) {
+      setCursor(cursorOffChar)
+    }
+  })
+
+  return (
+    <CoordinateText
+      position={{x: 50, y: 50}}
+      color={COLORS.BITCOIN}
+      text={`test${cursor}`}
+      align='left'
+    />
+  )
 }

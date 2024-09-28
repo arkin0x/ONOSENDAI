64a242393ac433d54304a186fde2c3b4bbcb9d14
arkin0x <arkinox@pm.me>
2024-09-28 15:31:01 -0500

    beh

--- src/store/BuilderStore.ts
+++ src/store/BuilderStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { createJSONStorage, persist, PersistStorage } from 'zustand/middleware'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 export interface Vertex {
   id: string

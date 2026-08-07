17ebf180f58e52136b3255a591716ff359228bb8
arkin0x <arkinox+onosendai@pm.me>
2026-08-07 04:43:43 +0000

    fix(terrain): enable vertexColors for instance color rendering

--- src/scene/TerrainField.tsx
+++ src/scene/TerrainField.tsx
@@ -144,12 +144,11 @@ export function TerrainField({ field, fadeDirection, fadeDuration = 0.5, onFadeC
       <sphereGeometry args={[1, 16, 16]} />
       <meshStandardMaterial
         ref={materialRef}
+        vertexColors
         transparent
         opacity={initialOpacity}
         roughness={0.3}
         metalness={0.1}
-        emissive="#000000"
-        emissiveIntensity={0.15}
         toneMapped={false}
       />
     </instancedMesh>

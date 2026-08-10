8f2d4e088480bda6d05753d8a57e5791a7df0c00
arkin0x <arkinox+onosendai@pm.me>
2026-08-10 19:24:20 +0000

    fix: anchor avatar and cursor to avatar position, not viewCenter
    The avatar was rendering offset from origin because it used viewCenter()
    which returns cursor position when active. This made the entire scene
    appear to follow the cursor even though terrain was anchored to avatar.
    
    Changes:
    - Avatar.tsx: always render at origin (0,0,0), remove axes prop
    - Cursor.tsx: use position instead of viewCenter for coordinate origin
    - Scene.tsx: remove axes prop from Avatar
    
    Now avatar, terrain, and cursor tether all share the same anchor point:
    the avatar's world position.

--- src/scene/Avatar.tsx
+++ src/scene/Avatar.tsx
@@ -4,36 +4,22 @@
  * A red wireframe icosahedron marks your exact position in cyberspace.
  * This is the same avatar shape from Onosendai v1, representing your
  * presence at this coordinate.
+ *
+ * Since the terrain is anchored to avatar position, the avatar always
+ * renders at the origin (0,0,0) of the world group.
  */
 
 import { useMemo } from 'react'
 import { IcosahedronGeometry, EdgesGeometry } from 'three'
-import { cellOffset, type ViewAxes } from '../lib/space'
-import { alignedOrigin, useCyberspace } from '../store/useCyberspace'
 
-interface Props {
-  axes: ViewAxes
-}
-
-export function Avatar({ axes }: Props): JSX.Element {
-  const position = useCyberspace((s) => s.position)
-  const viewCenter = useCyberspace((s) => s.viewCenter())
-  const scaleExp = useCyberspace((s) => s.scaleExp)
-
-  const [ax, ay] = useMemo(() => {
-    const origin = alignedOrigin(viewCenter, scaleExp)
-    const ax = cellOffset(position[axes.right.axis], origin[axes.right.axis], scaleExp, axes.right.dir)
-    const ay = cellOffset(position[axes.up.axis], origin[axes.up.axis], scaleExp, axes.up.dir)
-    return [ax, ay]
-  }, [position, viewCenter, scaleExp, axes])
-
+export function Avatar(): JSX.Element {
   const avatarGeometry = useMemo(() => {
     const geo = new IcosahedronGeometry(0.5, 1)
     return new EdgesGeometry(geo)
   }, [])
 
   return (
-    <group position={[ax, ay, 0.1]}>
+    <group position={[0, 0, 0.1]}>
       <lineSegments geometry={avatarGeometry} frustumCulled={false}>
         <lineBasicMaterial color="#ff2323" toneMapped={false} />
       </lineSegments>
